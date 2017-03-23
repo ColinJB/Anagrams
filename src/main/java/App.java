d2a3e9be9307acd684bf37a85b6e7edd0657adb1
Colin Bloom <colinjbloom113@gmail.com>
2017-03-23 12:20:05 -0700

    final commit, add user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,14 @@ import java.io.Console;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Anagrams!");
-  } 
+    Console userConsole = System.console();
+    System.out.println("Welcome to the Anagram checker! Enter two words, and I will tell you if they are anagrams!");
+    System.out.println("Please enter the first word.");
+    String word1 = userConsole.readLine();
+    System.out.println("Please enter the second word.");
+    String word2 = userConsole.readLine();
+    Anagrams anagrams = new Anagrams();
+    boolean result = anagrams.anagramCheck(word1, word2);
+    System.out.println(result);
+  }
 }
